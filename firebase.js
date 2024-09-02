ed2e62c2035dacefb54c932e84b97819636ab97b
Shravya Reddy Akmy <akmyshravyareddy@gmail.com>
2024-09-01 18:41:50 -0700

    up-to-date

--- firebase.js
+++ firebase.js
@@ -6,12 +6,12 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC_45XgiM_cTv3XH4Dv0bQ2NADsMu_3-b4",
-  authDomain: "club3-635d3.firebaseapp.com",
-  projectId: "club3-635d3",
-  storageBucket: "club3-635d3.appspot.com",
-  messagingSenderId: "859275643862",
-  appId: "1:859275643862:web:dbaf3f8b068d55cb247277"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
